b7ae2373a55ec9284ac0811558db0d9869736616
Evan Irving-Pease <evan.irvingpease@gmail.com>
2020-02-24 12:17:42 +0000

    tidy up of columns names

--- scripts/entrez_select_seqs.py
+++ scripts/entrez_select_seqs.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+
 import pandas as pd
 
 
-def entrez_select_taxa_seqs(accesions_file, taxa_file, output_file):
-    accessions = pd.read_csv(accesions_file, sep='\t', skiprows=[0], header=None,
-                             names=['accession', 'taxid', 'org_name', 'defline', 'length'], dtype={'taxid': 'str'})
+def entrez_select_taxa_seqs(nuccore_file, taxa_file, output_file):
 
-    taxa = pd.read_csv(taxa_file, sep='\t', skiprows=[0],
-                       header=None, names=['taxid', 'genus', 'family', 'species', 'subspecies'], dtype={'taxid': 'str'})
+    accessions = pd.read_csv(nuccore_file, sep='\t')
+    taxa = pd.read_csv(taxa_file, sep='\t')
 
     print("read the accessions and the taxa", file=sys.stderr)
 
-    taxaccessions = pd.merge(accessions, taxa, on=['taxid'], how='outer')
+    # TODO make rank configurable in the config.yaml file
+    rank = 'species'
 
-    taxaccessions = taxaccessions[~taxaccessions['species'].isnull()]
+    taxaccessions = pd.merge(accessions, taxa, on=['TSeq_taxid'], how='outer')
 
+    # TODO if rank==subspecies, you must issue a warning that the resultset sizes are different
+    taxaccessions = taxaccessions[~taxaccessions[rank].isnull()]
+
+    # TODO groupby on species name but species are not unique
     selected_sequences = taxaccessions.loc[
-        taxaccessions.groupby('species')['length'].idxmax(), ['species', 'accession']]
+        taxaccessions.groupby(rank)['TSeq_length'].idxmax(), ['TSeq_orgname', 'TSeq_accver']]
 
     print("selected the longest sequence per species, writing it to a file", file=sys.stderr)
 
-    selected_sequences.to_csv(output_file, sep='\t', header=False, index=False)
+    selected_sequences.to_csv(output_file, sep='\t', header=True, index=False)
 
 
 if __name__ == '__main__':
@@ -29,7 +35,7 @@ if __name__ == '__main__':
     sys.stderr = open(snakemake.log[0], 'w')
 
     entrez_select_taxa_seqs(
-        accesions_file=snakemake.input[0],
+        nuccore_file=snakemake.input[0],
         taxa_file=snakemake.input[1],
         output_file=snakemake.output[0]
     )
--- scripts/entrez_taxonomy_query.py
+++ scripts/entrez_taxonomy_query.py
@@ -24,7 +24,7 @@ def entrez_taxonomy_query(config, nuccore_file, output_file):
     accessions = pd.read_csv(nuccore_file, sep='\t', usecols=['TSeq_taxid'], squeeze=True).unique()
 
     with open(output_file, 'w') as fout:
-        fieldnames = ['TaxId', 'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'subspecies']
+        fieldnames = ['TSeq_taxid', 'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species', 'subspecies']
         w = csv.DictWriter(fout, fieldnames, delimiter='\t', extrasaction="ignore")
         w.writeheader()
 
@@ -34,7 +34,7 @@ def entrez_taxonomy_query(config, nuccore_file, output_file):
 
             for node in records:
                 taxon = dict()
-                taxon['TaxId'] = node['TaxId']
+                taxon['TSeq_taxid'] = node['TaxId']
                 for item in node['LineageEx']:
                     if item["Rank"] in fieldnames:
                         taxon[item["Rank"]] = item['ScientificName']
