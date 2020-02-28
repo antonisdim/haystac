aa3a9826d3170e1b2bc8fab7182a82b9ace63e29
Evan Irving-Pease <evan.irvingpease@gmail.com>
2020-02-28 15:30:15 +0000

    extract the taxon id from the overly complicated nesting in the GBSeq XML format

--- scripts/test_esearch.py
+++ scripts/test_esearch.py
@@ -3,7 +3,6 @@
 
 import os
 import sys
-
 from pprint import pprint
 
 from Bio import Entrez
@@ -16,8 +15,28 @@ Entrez.email = 'noreply@example.com'
 entrez_query = 'NZ_CP019458.1[Accession] OR NZ_CP021748.1[Accession] OR NZ_LT670818.1[Accession] ' \
                'OR NZ_CP022415.1[Accession] OR NZ_FOYO01000001.1[Accession] OR NZ_CP020121.1[Accession]'
 
-retmax = 1
+retmax = 2
 counter = 0
+
+
+def gen_dict_extract(key, var):
+    """Find keys in nested dictionaries"""
+    if hasattr(var, 'items'):
+        for k, v in var.items():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in gen_dict_extract(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in gen_dict_extract(key, d):
+                        yield result
+    elif isinstance(var, list):
+        for d in var:
+            for result in gen_dict_extract(key, d):
+                yield result
+
 
 while True:
     handle = Entrez.esearch(db=ENTREZ_DB_NUCCORE, term=entrez_query, retmax=retmax, idtype="acc", usehistory='Y',
@@ -29,11 +48,15 @@ while True:
         break
 
     # TODO set the following to save downloading the full sequence each time
-    records = guts_of_entrez(ENTREZ_DB_NUCCORE, ENTREZ_RETMODE_XML, ENTREZ_RETTYPE_GB, handle['IdList'], retmax)
+    records = list(guts_of_entrez(ENTREZ_DB_NUCCORE, ENTREZ_RETMODE_XML, ENTREZ_RETTYPE_GB, handle['IdList'], retmax))
 
-    print(list(records))
+    for record in records:
+        # extract the taxon id from the overly complicated nesting in the GBSeq XML format
+        record['TSeq_taxid'] = [val.replace('taxon:', '')
+                                for val in gen_dict_extract('GBQualifier_value', record) if 'taxon:' in val].pop()
 
-    pprint(handle['IdList'])
+    pprint(records)
+
     counter += 1
 
     break
