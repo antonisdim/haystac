bf24b48a196eb9e0b7b6d58a6662bc1aa1774d66
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-03-28 16:09:51 +0000

    Attempt to fix the error handling in entrez_download_sequences.py

--- scripts/entrez_download_sequence.py
+++ scripts/entrez_download_sequence.py
@@ -14,9 +14,10 @@ sys.path.append(os.getcwd())
 from scripts.entrez_utils import guts_of_entrez, ENTREZ_DB_NUCCORE, ENTREZ_RETMODE_TEXT, ENTREZ_RETTYPE_FASTA
 
 TOO_MANY_REQUESTS_WAIT = 5
+MAX_RETRY_ATTEMPTS = 3
 
 
-def entrez_download_sequence(accession, config, output_file):
+def entrez_download_sequence(accession, config, output_file, attempt=1):
     """
     Fetch the reference genome from NCBI.
     """
@@ -33,12 +34,19 @@ def entrez_download_sequence(accession, config, output_file):
 
     except(urllib.error.HTTPError, e):
         if e.code == 429:
-            time.sleep(TOO_MANY_REQUESTS_WAIT)
-            records = guts_of_entrez(ENTREZ_DB_NUCCORE, ENTREZ_RETMODE_TEXT, ENTREZ_RETTYPE_FASTA, [accession],
-                                     batch_size=1)
-            with gzip.open(output_file, 'wt') as fout:
-                for fasta in records:
-                    fout.write(fasta)
+
+            attempt += 1
+
+            if attempt > MAX_RETRY_ATTEMPTS:
+                print("Exceeded maximum attempts {}...".format(attempt), file=sys.stderr)
+                return None
+            else:
+                time.sleep(TOO_MANY_REQUESTS_WAIT)
+                entrez_download_sequence(accession, config, output_file, attempt)
+
+        else:
+            raise RuntimeError("There was a urllib.error.HTTPError with code {}".format(e))
+
 
 
 if __name__ == '__main__':
