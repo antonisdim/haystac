1976a58979e85ce4dcc003db0bd1a4f1a3ee900e
Evan Irving-Pease <evan.irvingpease@gmail.com>
2020-07-23 13:35:18 +0100

    update the CLI interface design

--- rip_example_commands.sh
+++ rip_example_commands.sh
@@ -1,143 +1,139 @@
-# only fetch genomes
-rip database_build \
---query_name <str> \
---entrez_query <str>  \
---entrez_email <str> \
---entrez_batchsize <int> \
---entrez_rank <str> \
---custom_seq_file <str> \
---custom_acc_file <str> \
---mem_mb <float> \
---mem_rescale_factor <float> \
---with_entrez_query \
---with_refseq_rep \
---build_db
+# the first time you run... prompt for email address
+
+# include ALL persistent config settings
+rip config
+  # TODO mandatory
+  --email '<str>'
+  --cache '/path/to/what/used/to/be/database'  # TODO default to ~/rip/
+
+  # TODO optional
+  --batchsize '<int>'
+  --bowtie2-scaling  '<int>'  # mem-rescaling
+  --bowtie2-treads   '<int>'  # needs to be documented that this effects --mem-scaling
+  --mismatch_probability '<float>'
+
+  --etc...
+
+# outputs to > ~/.rip/config.yaml
+
+#########################
+
+# default flags, available for ALL commands
+  --cores '<int>'    # default to ALL cores
+  --memory '<float>' # default to ALL memory
+  --snakemake "--any-smk-flag --etc -blah"  # none of these options are documented, but we can link to the smk CLI in our docs
+
+#########################
+
+rip database --help
 
 # Up to the bowtie2 indices for the individual genomes
-rip database_build \
---query_name <str> \
---entrez_query <str>  \
---entrez_email <str> \
---entrez_batchsize <int> \
---entrez_rank <str> \
---custom_seq_file <str> \
---custom_acc_file <str> \
---mem_mb <float> \
---mem_rescale_factor <float> \
---with_entrez_query \
---with_refseq_rep \
---index_db
+rip database
+  --mode fetch
 
-# Up to the bowtie2 filtering aln idx 
-rip database_build \
---query_name <str> \
---entrez_query <str>  \
---entrez_email <str> \
---entrez_batchsize <int> \
---entrez_rank <str> \
---custom_seq_file <str> \
---custom_acc_file <str> \
---mem_mb <float> \
---mem_rescale_factor <float> \
---with_entrez_query \
---with_refseq_rep \
---aln_filter_index
+  --output '/path/to/what/used/to/be/query_name'
+  --refseq-rep              # optional, default False
+  --query '<str>'           # optional, default ""
+  --rank '<str>'            # optional, default "species"
+  --sequences '<filename>'  # optional, default ""
+  --accessions '<filename>' # optional, default ""
+
+# if you run fetch, it must produce a settings file
+
+# Up to the bowtie2 filtering aln idx
+rip database
+  --mode index
+  --output '/path/to/db/name'
+
+#########################
 
 # Do everything db related
-rip database_build \
---query_name <str> \
---entrez_query <str>  \
---entrez_email <str> \
---entrez_batchsize <int> \
---entrez_rank <str> \
---custom_seq_file <str> \
---custom_acc_file <str> \
---mem_mb <float> \
---mem_rescale_factor <float> \
---with_entrez_query \
---with_refseq_rep \
---complete_db_creation
+rip database
+  --mode build              # optional, default "build"
+
+  --output '/path/to/db'
+  --rank '<str>'            # optional, default "species"
+  --refseq-rep              # optional, default False
+  --query '<str>'           # optional, default ""
+  --sequences '<filename>'  # optional, default ""
+  --accessions '<filename>' # optional, default ""
+
+# must have at least one of (--refseq-rep / --query / -sequences / --accessions)
+
+# if you run build, it must produce a settings file
+# if you run build... you must not have first run fetch
+# if you run either fetch or build, you cannot run a second time with the same ID
+
+################################################################################
 
 # Only trim adapters
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---data_preprocess
+rip sample
+  --output '/path/to/sample'
+
+  --fastq '<str>'           # optional, default ""
+  --fastq-r1 '<str>'        # optional, default ""
+  --fastq-r2 '<str>'        # optional, default ""
+  --sra '<str>'             # optional, default ""
+  --collapse '<str>'        # optional, default False
+  --trim-adapters           # optional, default True
+  --adapter-remove-flags    # TODO Antony to add flags for specifying adapter sequences
+
+# must provide either (--fastq XOR (--fastq-r1 AND --fastq-r2) XOR --sra)
+# --collapse can only be used with (--fastq-r1 AND --fastq-r2)
+# --sra should know if PE or SE (will have to query the metadata)
+
+# TODO do we want to run fastqc here?
+# TODO make sure that this produces ALL the sample specific targets
+
+################################################################################
 
 # Only do everything up to the filtering alignment 
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---aln_filter
+rip analyse
+  --mode filter  # TODO find better name for this
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --output '/path/to/results'
+
+# --database must existing and be complete!
+# --sample "preprocess" must be complete (otherwise throw error)
 
 # Only do everything up to the metagenomic alignments
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---aln_meta
+rip analyse
+  --mode align
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --output '/path/to/results'
+
+# Only do everything up to the metagenomic alignments
+rip analyse
+  --mode likelihoods
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --output '/path/to/results'
+
+#########################
 
 # Only do everything up to calculating the assignment probabilities
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---probabilities
+rip analyse
+  --mode probabilities
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --genera '<str1 str2 str3 ...>'  # optional, default ""
+  --output '/path/to/results'
 
 # Only do everything up to calculating the posterior abundances
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---abundances
+rip analyse
+  --mode abundances
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --genera '<str1 str2 str3 ...>'  # optional, default ""
+  --output '/path/to/results'
 
 # Only do everything up to mapdamage analysis
-rip analyse_sample \
---sample_name <str> \
---fastq <str> \
---fastq_R1 <str> \
---fastq_R2 <str> \
---mismatch_probability <float> \
---bowtie2_treads <int> \
---sra <str> \
---input_mode <str> \
---trim_adapters \
---specific_genera <str1 str2 str3 ...> \
---mapdamage
+rip analyse
+  --mode mapdamage
+  --database '/path/to/database'
+  --sample '/path/to/sample'
+  --genera '<str1 str2 str3 ...>'  # optional, default ""
+  --output '/path/to/results'
+
