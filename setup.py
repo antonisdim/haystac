8065b69624a2d27cc7431e265f45bfb210c3cf2f
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-10-08 12:30:00 +0100

    find_namespace_packages()

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/antonisdim/rip",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_namespace_packages(),
     classifiers=["Programming Language :: Python :: 3",
                  "License :: MIT License"],
 )
