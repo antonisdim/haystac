649f264bb39967619b47b5c3f6f80fd422293043
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2020-12-05 18:22:55 +0000

    haystac in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     required = [x for x in f.read().splitlines() if not x.startswith("#")]
 
 setuptools.setup(
-    name="haystac_metagenomics",
+    name="haystac",
     version="0.1",
     author="Evangelos A. Dimopoulos, Evan K. Irving-Pease",
     author_email="antonisdim41@gmail.com",
