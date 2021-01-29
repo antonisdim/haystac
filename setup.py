41d0a460976c943878d43eb66742458983c686fc
antonisdim <evangelos-antonios.dimopoulos@hertford.ox.ac.uk>
2021-01-29 11:23:21 +0000

    New version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="haystac",
-    version="0.2",
+    version="0.3",
     author="Evangelos A. Dimopoulos, Evan K. Irving-Pease",
     author_email="antonisdim41@gmail.com",
     description="Species identification pipeline for both single species and metagenomic samples.",
